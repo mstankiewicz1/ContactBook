c0ea3f1eb14721b252fbe5d5b428f50d4ee83114
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-07-29 00:50:22 +0200

    Postepy 29.07.19 godz 00:50

---
+++ js/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>ContactBook</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
