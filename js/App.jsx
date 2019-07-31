315d852607368d7446e5d26693d89883ee73941a
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-07-31 23:34:32 +0200

    Postępy 31.07.19r godz 23:34

--- js/App.jsx
+++ js/App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>ContactBook</h1>
             </div>
         )
